662672fe83960be88d41d61295d889898aa7dda9
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2017-04-07 17:42:00 +0500

    getting simple @lonitude & @latitude in android simple example using @GOogleAPiClient & @LocationServies

--- mapInitial/app/src/main/java/com/example/asheransari/mapinitial/MainActivity.java
+++ mapInitial/app/src/main/java/com/example/asheransari/mapinitial/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.asheransari.mapinitial;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private String LOG = "MainActivity";
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API).addOnConnectionFailedListener(this).build();
+        mTextView = (TextView) findViewById(R.id.textOutputView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        mLocationRequest.getNumUpdates();
+        mLocationRequest.setInterval(1000);//its means every secong.. 1000ms = 1sec;
+        //yeha pe hum callBack lagaen ge..
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e(LOG,"COnnection has been suspended...");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(LOG,"Connection has been failed");
+    }
+    @Override
+    public void onLocationChanged(Location location){
+        Log.e(LOG,location.toString());
+        mTextView.setText(location.toString());
+        mTextView.append("\nLatitude = "+location.getLatitude()+" & Longitude = "+location.getLongitude());
     }
 }
